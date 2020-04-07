1c692b4c6597055a720a359c7609ada42f6733ab
Anatolie1 <gavriluta.anatolie@gmail.com>
2020-04-07 17:20:33 +0200

    a

---
+++ Model/Shop.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MvvmQuete
+{
+    public class Shop : INotifyPropertyChanged
+    {
+        private int shopId;
+        private string name;
+        private string country;
+
+        public int ShopId
+        {
+            get
+            {
+                return shopId;
+            }
+            set
+            {
+                shopId = value;
+                OnPropertyChanged("ShopId");
+            }
+        }
+        public string ShopName
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                OnPropertyChanged("ShopName");
+            }
+        }
+       
+        public string Country
+        {
+            get
+            {
+                return country;
+            }
+            set
+            {
+                country = value;
+                OnPropertyChanged("Country");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ ViewModel/ShopViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MvvmQuete
+{
+    public class ShopViewModel
+    {
+        public List<Shop> Shops { get; set; } = new List<Shop>();
+
+        public ShopViewModel()
+        {
+            IList<Shop> shopsList = new List<Shop>
+            {
+                new Shop{ShopId=1,ShopName="MC",Country="FRANCE"},
+                new Shop{ShopId=2,ShopName="AL",Country="USA"},
+                new Shop{ShopId=3,ShopName="GE",Country="GERMANY"},
+                new Shop{ShopId=4,ShopName="MI",Country="SPAIN"},
+                new Shop{ShopId=5,ShopName="LC",Country="ITALY"},
+                new Shop{ShopId=6,ShopName="VB",Country="RUSSIA"},
+                new Shop{ShopId=7,ShopName="WX",Country="AUSTRALIA"},
+                new Shop{ShopId=8,ShopName="AR",Country="INDIA"}
+            };
+            Shops.AddRange(shopsList);
+        }        
+    }
+}
